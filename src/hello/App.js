3245fd8e40fe54af89a37196eeb912d77e09f2d7
onlyforhebe <342552254@qq.com>
2017-03-01 18:46:07 +0800

    增加Ant Design

--- src/hello/App.js
+++ src/hello/App.js
@@ -1,20 +1,33 @@
 import React, {
   Component
-} from 'react';
-import logo from './logo.svg';
-import './App.css';
+} from 'react'
+import {
+  Tabs,
+  Icon
+} from 'antd'
+import logo from './logo.svg'
+import './App.css'
+
+const TabPane = Tabs.TabPane;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hello, React! Glad to see you again!</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Tabs defaultActiveKey="1">
+          <TabPane tab={<span><Icon type="apple" />Tab 1</span>} key="1">
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Hello, iOS!</h2>
+            </div>
+          </TabPane>
+          <TabPane tab={<span><Icon type="android" />Tab 2</span>} key="2">
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Hello, Android!</h2>
+            </div>
+          </TabPane>
+        </Tabs>
       </div>
     );
   }
